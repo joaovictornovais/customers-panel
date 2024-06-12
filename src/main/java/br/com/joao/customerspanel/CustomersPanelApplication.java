f8abdc90187e5d35ad1f7b186bf1c5d1e61b9311
João Victor <joao.dipaay@gmail.com>
2024-06-12 20:45:43 -0300

    initial config

---
+++ src/main/java/br/com/joao/customerspanel/CustomersPanelApplication.java
@@ -0,0 +1,13 @@
+package br.com.joao.customerspanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersPanelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomersPanelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/joao/customerspanel/CustomersPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joao.customerspanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersPanelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
